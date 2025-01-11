b1d30f48b8928a54bb7a6f9ae0a480714405d395
Furk <furkanakmantar@gmail.com>
2025-01-11 12:10:50 +0100

    Created FAQ Page

---
+++ ExchangeProgram/Pages/FAQ.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExchangeProgram.Pages
+{
+    public class FAQModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
